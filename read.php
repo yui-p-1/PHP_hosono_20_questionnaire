7422ccb97bc6c94ca95eef96b051129e2bbd7f2d
yui <hosono.yui@outlook.jp>
2023-01-14 17:58:32 +0900

    17:58 正常動作

--- read.php
+++ read.php
@@ -1,5 +1,38 @@
 <?php
+// XSS対策用関数作成
+function h ($value) {
+    return htmlspecialchars($value,ENT_QUOTES);
+}
+?>
 
+<table>
+<?php
+// ファイルを変数に格納
+$fp = fopen("data/data.txt","r");
+
+while(!feof($fp)){
+    $line = fgets($fp);
+    if (trim($line) != null){
+        list($date_1, $date_2, $name, $mail) = explode(" ", $line);
+?>
+
+<tr>
+<td><?php print $date_1 ?></td>
+<td><?php print $date_2 ?></td>
+<td><?php print $name ?></td>
+<td><?php print $mail ?></td>
+</tr>
+<?php
+    }
+}
+fclose($fp);
+?>
+</table>
+
+
+
+
+/*
 // XSS対策用関数作成
 function h ($value) {
     return htmlspecialchars($value,ENT_QUOTES);
@@ -25,5 +58,4 @@ while (!feof($fp)) {
 // fcloseでファイルを閉じる
 fclose($fp);
 
-
-?>
+*/
--- write.php
+++ write.php
@@ -5,12 +5,13 @@ $name = $_POST["name"];
 $mail = $_POST["mail"];
 
 // 申込時間取得
-$date = date("Y/m/d H:i:s");
+$date_1 = date("Y/m/d");
+$date_2 = date("H:i:s");
 
 // ファイルを読み込む
 $file = fopen("./data/data.txt","a");
 // ファイルに書き込む
-fwrite($file,$date." ".$name." ".$mail."\n");
+fwrite($file,$date_1." ".$date_2." ".$name." ".$mail."\n");
 // ファイルを閉じる
 fclose($file);
 
