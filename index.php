600dd4df647ea7e760b6ad617ec50667d6b796d4
yui <hosono.yui@outlook.jp>
2023-01-14 18:15:02 +0900

    18:15 正常動作

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>アンケート入力</title>
+    <link rel="stylesheet" href="./css/sample.css">
 </head>
+
 <body>
     <h1>アンケートフォーム</h1>
     <form method="POST" action="write.php">
